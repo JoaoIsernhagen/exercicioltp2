8744e577345a39dffc9ab0d3bd89dde73a5e331c
Lucas de Cahe Cruz Isernhagen <lucasdecahe@hotmail.com>
2022-12-05 23:36:39 -0300

    Correção

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,12 +1,10 @@
 package aplication;
 
-import entities.EscolhaVeiculos;
 import entities.Produc;
 
 import services.Veiculos;
 
 import java.util.Locale;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Program {
@@ -16,83 +14,37 @@ public class Program {
         Scanner sc = new Scanner(System.in);
 
         Produc produc = new Produc();
-        EscolhaVeiculos escolhaVeiculos = new EscolhaVeiculos();
 
         System.out.println("Seu Nome:");
         produc.name = sc.nextLine();
 
-        String opcao2 = null;
+        char ehFinalizar;
 
         do {
+            Veiculos.EscolheVeiculos(sc);
 
-            char veiculo = Veiculos.EscolheVeiculos();
+            sc = new Scanner(System.in);
 
+            System.out.printf("%n%n");
+            System.out.printf("Deseja iniciar outra simulaçã0: (S - Sim | N - Nâo) %n");
+            ehFinalizar = sc.nextLine().toUpperCase().charAt(0);
 
-            if (veiculo == 'O') {
+        } while (ehFinalizar == 'S');
 
-                System.out.println("Valor da Parcela Mensal?");
-                produc.parcelaOnibus = sc.nextDouble();
-                System.out.println("Gasto semestral com onibus e " + produc.valorOnibus());
-            }
+        sc.close();
+           
 
-            if (veiculo == 'O') {
-                System.out.println("Dejesa escolher outro veiculo? S para sem | N para nao");
-                 opcao2 = sc.nextLine();
-                
-            }
-        } while ( opcao2 == "S");
-
-
-            boolean combustiveValido = true;
-
-            do {
-
-                sc.nextLine();
-                System.out.println("Qual combustivel so seu " + escolhaVeiculos.MeioTransporte + ":");
-                System.out.println("G - Gasolina | A - Alcool");
-                char combustivel = sc.nextLine().toUpperCase().charAt(0);
-
-                produc.selecionarCombustivel(combustivel);
-
-                if (Objects.equals(produc.combustivel, "")) {
-                    System.out.println("Combustivel invalido");
-                } else {
-                    combustiveValido = false;
-                }
+            // System.out.println("---------------------------------------:");
+            // System.out.println("            Informações:");
+            // System.out.println("---------------------------------------:");
+            // System.out.println("Aluno: " + produc.name);
+            // System.out.println("Meio de Transporte: " + escolhaVeiculos.MeioTransporte);
+            // System.out.printf("Valor do gasto diario com Combustivel %.2f%n", produc.gastoCombutivelDia());
+            // System.out.printf("Valor do gasto semestral com Combustivel %.2f%n", produc.gastoSemestral());
 
 
-            } while (combustiveValido);
-
-
-            if (Objects.equals(produc.combustivel, "G")) {
-                System.out.println("Preco Atual da" + produc.combustivel);
-                produc.gasolina = sc.nextDouble();
-                produc.valorcombustivel = produc.gasolina;
-            } else {
-                System.out.println("Preco atual do " + produc.combustivel);
-                produc.alcool = sc.nextDouble();
-                produc.valorcombustivel = produc.alcool;
-            }
-
-            System.out.println("Media quilometros por litros?");
-            produc.media = sc.nextDouble();
-
-
-            System.out.println("Distancia entre origem e destino?");
-            produc.distancia = sc.nextDouble();
-
-
-            System.out.println("---------------------------------------:");
-            System.out.println("            Informações:");
-            System.out.println("---------------------------------------:");
-            System.out.println("Aluno: " + produc.name);
-            System.out.println("Meio de Transporte: " + escolhaVeiculos.MeioTransporte);
-            System.out.printf("Valor do gasto diario com Combustivel %.2f%n", produc.gastoCombutivelDia());
-            System.out.printf("Valor do gasto semestral com Combustivel %.2f%n", produc.gastoSemestral());
-
-
-        }
     }
+}
 
 
 
--- src/services/Veiculos.java
+++ src/services/Veiculos.java
@@ -1,15 +1,18 @@
 package services;
 
+import java.util.Scanner;
 
-public static class Veiculos {
+public final class Veiculos {
 
-    public static char EscolheVeiculos()
+    public static final int MESESLETIVOS = 12;
+
+    public static void EscolheVeiculos(Scanner sc)
     {
         boolean ehValido = false;
-        char veiculo = '';
+        char veiculo;
 
         do {
-            System.out.printf("Escolha o veiculo(C - Carro | M - Moto | O - Onibus %n");
+            System.out.printf("Escolha o veiculo:(C - Carro | M - Moto | O - Onibus) %n");
             veiculo = sc.nextLine().toUpperCase().charAt(0);
 
             if (ValidarVeiculoSelecionado(veiculo)) {
@@ -19,10 +22,12 @@ public static class Veiculos {
             }
         } while (!ehValido);
 
-        return veiculo;
+        if(veiculo == 'C') CalculaValoresCarro(sc);
+        if(veiculo == 'M') CalculaValoresMoto(sc);
+        if(veiculo == 'O') CalculaValoresOnibus(sc);   
     }
 
-    private boolean ValidarVeiculoSelecionado(char veiculo)
+    private static boolean ValidarVeiculoSelecionado(char veiculo)
     {
         if(veiculo == 'C' || veiculo == 'M' || veiculo == 'O' )
         {
@@ -33,4 +38,35 @@ public static class Veiculos {
         }
     }
 
+    private static void CalculaValoresOnibus(Scanner sc)
+    {
+        double vlrPassagem = 0;
+
+        do
+        {
+            System.out.println("Digite o valor da mensalidade?");
+            vlrPassagem = sc.nextDouble();
+
+            if(vlrPassagem > 0)
+            {
+                double valor = vlrPassagem * MESESLETIVOS;
+                System.out.println("Valor gasto em passagens será de: " + valor);
+            }
+            else
+            {
+                System.out.println("Valor da passagem deve ser maior que 0!");
+            }
+
+        } while(vlrPassagem <=0);
+    }
+
+    private static void CalculaValoresCarro(Scanner sc)
+    {
+
+    }
+
+    private static void CalculaValoresMoto(Scanner sc)
+    {
+
+    }
 }
